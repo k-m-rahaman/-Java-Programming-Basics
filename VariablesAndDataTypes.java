e53c20a4ae42b45164d0ecc60f61b7a1ff8457a3
k-m-rahaman <kmrofficial786@gmail.com>
2026-05-29 05:48:35 +0530

    Added variables and data types program

---
+++ VariablesAndDataTypes.java
@@ -0,0 +1,16 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+
+        int age = 19;
+        double height = 5.8;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Kazi";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
