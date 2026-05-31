e1e577af2281f1b9566ed526aba649ff5016e5b6
k-m-rahaman <kmrofficial786@gmail.com>
2026-06-01 05:20:18 +0530

    Added methods program

---
+++ Methods.java
@@ -0,0 +1,13 @@
+public class Methods {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int result = add(5, 7);
+
+        System.out.println("Sum: " + result);
+    }
+}
