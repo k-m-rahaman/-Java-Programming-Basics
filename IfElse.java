2026c0d6d5ceb5d17b5c6f452e9040dee4c281aa
k-m-rahaman <kmrofficial786@gmail.com>
2026-05-31 06:33:18 +0530

    Added conditional statements program

---
+++ IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+    public static void main(String[] args) {
+
+        int number = -5;
+
+        if (number > 0) {
+            System.out.println("Positive Number");
+        } else if (number < 0) {
+            System.out.println("Negative Number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
